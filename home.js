8da1a43406761035f9384aebc728a3b8d472ab8e
NataSereda <natalya.romanyak@gmail.com>
2021-07-18 18:02:33 +0300

    function

---
+++ home.js
@@ -0,0 +1,31 @@
+
+/*створити функцію яка обчислює та повертає площу прямокутника висотою*/
+
+function calc(a, b) {
+    return a * b;
+}
+let calc1 = calc(10, 20)
+console.log(calc1);
+
+/*створити функцію яка обчислює та повертає площу кола*/
+
+function calcArea(r,pi =3.14)
+{return r*r*pi
+}
+let calcArea1 = calcArea(20,3.14)
+console.log(calcArea1);
+
+
+
+/*створити функцію яка обчислює та повертає площу циліндру*/
+
+function cylinder (r,h,pi =3.14) {
+    return 2*pi*r*h
+}
+let S = cylinder(5,8,3.14)
+console.log(S);
+
+
+/*створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше */
+
+
